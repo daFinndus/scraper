dcda1e47bc6dae91ba07c0a77335ef5779782fbf
daFinndus <finnlucajensen@proton.me>
2025-04-28 14:35:38 +0200

    feat: Initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+from flask import Flask, jsonify
+from bs4 import BeautifulSoup
+import requests
+
+app = Flask(__name__)
+
+
+def scrape():
+    url = "https://studentenwerk.sh/de/mensaplandruck?ort=1&mensa=5&aus=2"
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    data = {}
+
+    days = soup.find_all("div", class_="tag_headline")
+    for day in days:
+        date = day.get("data-day")
+        menus = day.find_all("div", class_="mensa_menu_detail")
+
+        dishes = []
+        for menu in menus:
+            dish_name_div = menu.find("div", class_="menu_name menu_name_kleiner")
+            price_div = menu.find("div", class_="menu_preis")
+
+            dish_name = dish_name_div.text.strip() if dish_name_div else "No dish name"
+            prices = price_div.text.strip() if price_div else "No price"
+
+            dishes.append({
+                "dish": dish_name,
+                "prices": prices
+            })
+
+        data[date] = dishes
+
+    return data
+
+
+@app.route('/', methods=['GET'])
+def get():
+    menu = scrape()
+    return jsonify(menu)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
